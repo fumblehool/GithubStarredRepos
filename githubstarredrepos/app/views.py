a087fab112dc482631696abbdd82898d3025f896
fumblehool <daman.4880@gmail.com>
2016-10-29 21:52:41 +0530

    Server setup

---
+++ githubstarredrepos/app/views.py
@@ -0,0 +1,17 @@
+from flask import render_template, request, redirect, session
+from app import app
+
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def inter_error(e):
+    return render_template("500.html"), 500
