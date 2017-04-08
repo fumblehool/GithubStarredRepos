0556af39b8859b2571127ee70ba68fa9093b0cf9
fumblehool <daman.4880@gmail.com>
2017-04-08 09:49:32 +0530

    Implemented command-line options with flask-script

--- githubstarredrepos/app/__init__.py
+++ githubstarredrepos/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
-
+from flask.ext.script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 from app import views
--- githubstarredrepos/run.py
+++ githubstarredrepos/run.py
@@ -1,3 +1,3 @@
-from app import app
+from app import manager
 
-app.run()
+manager.run()
